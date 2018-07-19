2b6f5ccdda7b38419a4d4fdfe41d94217672c2a3
BeryNice <berenice.lemus@gmail.com>
2018-07-19 18:16:19 -0500

    agregando elementos para loguearse con facebook

--- src/js/main.js
+++ src/js/main.js
@@ -1,92 +1,139 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDVH3W9bV3MsZbXx4EqpV4Fg2RwlV0yhGM",
+  authDomain: "red-social-dc1b2.firebaseapp.com",
+  databaseURL: "https://red-social-dc1b2.firebaseio.com",
+  projectId: "rFed-social-dc1b2",
+  storageBucket: "red-social-dc1b2.appspot.com",
+  messagingSenderId: "159262835837"
+};
+firebase.initializeApp(config);
+
+
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
 //login es la instania del proveedor del servicio
 var provider = new firebase.auth.GoogleAuthProvider();
 function registrer() {
   //console.log('diste un click');
-  var email= document.getElementById('email').value;
-  var password= document.getElementById('password').value;
+  email.value;
+  password.value;
   firebase.auth().createUserWithEmailAndPassword(email, password)
-  .catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
 }
-function ingreso(){
-  var emailLogn= document.getElementById('emailLogn').value;
-  var passwordLogin= document.getElementById('passwordLogin').value;
-  firebase.auth().signInWithEmailAndPassword(emailLogn, passwordLogin).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
+
+
+
+//Intentando crear una función donde iniciar con facebook
+//FUNCION DE AUTENTICACIÓN CON FACEBOOK
+
+var providerFb = new firebase.auth.FacebookAuthProvider();
+function loginFacebook() {
+  //console.log('diste un click');
+  email.value;
+  password.value;
+  firebase.auth().signInWithPopup(provider).then(function (result) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
+
+// const loginFacebook = () => { }
+
+
+function ingreso() {
+  var emailLogn = document.getElementById('emailLogn').value;
+  var passwordLogin = document.getElementById('passwordLogin').value;
+  firebase.auth().signInWithEmailAndPassword(emailLogn, passwordLogin).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
 
 }
-function observador (){
-  firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    console.log('Existe Usuario Activo');
-    aparece();
-    // User is signed in.
-    var displayName = user.displayName;
-    var email = user.email;
-    var emailVerified = user.emailVerified;
-    var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
-    // ...
-  } else {
-    // User is signed out.
-    console.log('No Existe Usuario Activo');
-    // ...
-  }
-});
+function observador() {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      console.log('Existe Usuario Activo');
+      aparece();
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+    } else {
+      // User is signed out.
+      console.log('No Existe Usuario Activo');
+      // ...
+    }
+  });
 
 }
 observador();
 
-function aparece(){
+function aparece() {
   var contenido = document.getElementById('contenido');
   contenido.innerHTML = "Usuario activo";
 }
 
-$('#login').click(function(){
+$('#login').click(function () {
   firebase.auth()
     .signInWithPopup(provider)
-    .then(function(result){
+    .then(function (result) {
       console.log(result.user);
       guardarDatos(result.user);
       //hide propiedad para ocultar el boton
       $('#login').hide();
       //aqui colocamos la foto de google
-      $('#root').append("<img src='"+result.user.photoURL+"'/>");
-     });
+      $('#root').append("<img src='" + result.user.photoURL + "'/>");
+    });
 });
 // esta funion guarda los datos automaticamente
-function guardarDatos(user){
-  var usuario= {
-    uid:user.uid,
-    nombre:user.displayName,
-    email:user.email,
-    foto:user.photoURL
+function guardarDatos(user) {
+  var usuario = {
+    uid: user.uid,
+    nombre: user.displayName,
+    email: user.email,
+    foto: user.photoURL
   }
   firebase.database().ref("TalentMom/" + user.uid)
-  .set(usuario)
+    .set(usuario)
 }
 // aqui vamos a escribir en la base de datos
-$('#guardar').click(function(){
+$('#guardar').click(function () {
   firebase.database().ref("TalentMom")
-  .set({
-    nombre:"Linda",
-    edad:"20",
-    sexo:"Mujer"
-  })
+    .set({
+      nombre: "Linda",
+      edad: "20",
+      sexo: "Mujer"
+    })
 })
 // aki leo de la base de guardarDatos
 firebase.database().ref("TalentMom")
-.on("child_added",function(s){
-  var user= s.val();
-  $('#root').append("<img width='100px' src='"+user.foto+"'/>");
-})
+  .on("child_added", function (s) {
+    var user = s.val();
+    $('#root').append("<img width='100px' src='" + user.foto + "'/>");
+  })
