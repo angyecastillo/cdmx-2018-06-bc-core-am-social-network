be09bdc6e76418d958a24d2ee577bf1fe6ef85fc
BeryNice <berenice.lemus@gmail.com>
2018-07-27 09:32:30 -0500

    eliminando inicialización de firebase en autenticación

--- src/js/autenticacion.js
+++ src/js/autenticacion.js
@@ -1,15 +1,3 @@
-// Initialize Firebase
-const config = {
-    apiKey: "AIzaSyDVH3W9bV3MsZbXx4EqpV4Fg2RwlV0yhGM",
-    authDomain: "red-social-dc1b2.firebaseapp.com",
-    databaseURL: "https://red-social-dc1b2.firebaseio.com",
-    projectId: "rFed-social-dc1b2",
-    storageBucket: "red-social-dc1b2.appspot.com",
-    messagingSenderId: "159262835837"
-  };
-  firebase.initializeApp(config);
-
-
 // FUNCION DE REGISTRO CON EMAIL Y PASSWORD
   function registrer() {
     //console.log('diste un click');
